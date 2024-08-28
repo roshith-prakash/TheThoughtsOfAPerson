587cd6875633fbf9c22b722a1b8abb9b0f78f65b
roshith <roshithprakash7@gmail.com>
2024-08-28 13:50:27 +0530

    Minor look changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,40 +5,48 @@ import { toBeRead } from "../data/toBeRead.js";
 
 const Home = () => {
   return (
-    <div className="bg-home bg-cover bg-origin-center bg-no-repeat relative min-h-screen min-w-screen">
+    <div className="bg-home bg-cover bg-origin-center bg-no-repeat relative overflow-y-scroll h-screen min-w-screen">
       <Navbar />
-      <div className="py-10 px-5 md:px-10 flex flex-col gap-y-10 text-xl md:text-2xl text-justify">
+      <div className="py-10 px-5 md:px-10 flex flex-col gap-y-10 text-xl md:text-2xl text-justify font-medium">
         {/* Intro */}
-        <p className="px-8 py-10 bg-white bg-opacity-15 text-white rounded-lg italic">
+        <p className="px-8 py-10 bg-black bg-opacity-20 shadow text-white rounded-lg italic">
           Hey! I'm R, a part-time writer and fiction enthusiast. I mainly craft
           spoken word and freestyle poetry, with the occasional short story
           thrown in. I hope you enjoy your time here!
         </p>
 
         {/* Currently Reading */}
-        <div className="px-8 py-10 bg-white bg-opacity-15 text-white rounded-lg italic">
+        <div className="px-8 py-10 bg-black bg-opacity-20 shadow text-white rounded-lg italic">
           <p className="mb-8 text-3xl"> Currently Reading : </p>
           {currentlyReading}
         </div>
 
         {/* Books to be read */}
-        <div className="px-8 py-10 bg-white bg-opacity-15 text-white rounded-lg italic">
+        <div className="px-8 py-10 bg-black bg-opacity-20 shadow text-white rounded-lg italic">
           <p className="mb-8 text-3xl">To be read :</p>
 
-          <ol className="list-decimal pl-8 flex flex-col gap-y-3">
+          <ol className="flex flex-col gap-y-3 divide-y-2">
             {toBeRead.map((book, index) => {
-              return <li key={index}>{book}</li>;
+              return (
+                <li className="py-4 text-center" key={index}>
+                  {book}
+                </li>
+              );
             })}
           </ol>
         </div>
 
         {/* Books completed */}
-        <div className="px-8 py-10 bg-white bg-opacity-15 text-white rounded-lg italic">
+        <div className="px-8 py-10 bg-black bg-opacity-20 shadow text-white rounded-lg italic">
           <p className="mb-8 text-3xl">Books on the Shelf:</p>
 
-          <ol className="list-decimal pl-8 flex flex-col gap-y-3">
+          <ol className=" flex flex-col gap-y-3 divide-y-2">
             {completedBooks.map((book, index) => {
-              return <li key={index}>{book}</li>;
+              return (
+                <li className="py-4 text-center" key={index}>
+                  {book}
+                </li>
+              );
             })}
           </ol>
         </div>
