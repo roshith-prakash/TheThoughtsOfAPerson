dd5ad4a6f1c4e70022d02493a5b54c1461a5f70c
roshith-prakash <roshithprakash7@gmail.com>
2025-04-26 09:44:37 +0530

    Added leap of faith

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,4 +1,5 @@
 export const videos = [
   "https://www.youtube-nocookie.com/embed/fk8wa_0zpxQ?si=zR3BBrOkcRg3GblM",
+  "https://www.youtube.com/embed/O_SwWfGEfqc?si=5N1kf68fsTtdRbrR",
   "https://www.youtube.com/embed/qFRqZfQJ7NM?si=er7mLp0xddV4kVND",
 ];
