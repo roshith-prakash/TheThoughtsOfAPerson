10d67c44475c38ee7e272265f414af15db446706
roshith-prakash <roshithprakash7@gmail.com>
2025-06-10 12:21:04 +0530

    Added new video

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,5 +1,6 @@
 export const videos = [
-  "https://www.youtube-nocookie.com/embed/fk8wa_0zpxQ?si=zR3BBrOkcRg3GblM",
-  "https://www.youtube.com/embed/O_SwWfGEfqc?si=5N1kf68fsTtdRbrR",
+  "https://www.youtube.com/embed/M5UAZ7B4S00?si=vYl-mdELFffyY58e",
   "https://www.youtube.com/embed/qFRqZfQJ7NM?si=er7mLp0xddV4kVND",
+  "https://www.youtube.com/embed/O_SwWfGEfqc?si=5N1kf68fsTtdRbrR",
+  "https://www.youtube-nocookie.com/embed/fk8wa_0zpxQ?si=zR3BBrOkcRg3GblM",
 ];
